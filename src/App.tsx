889a286b6601340c1c46a5ae096d73659727e0fa
jmsandiegoo <jmsandiego0710@gmail.com>
2024-12-16 21:03:51 +0800

    Add basic design system tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl text-red-400 font-bold underline">
+      <h1 className="text-3xl text-primary text-body-regular font-bold underline">
         Hello world!
       </h1>
       <h1>Vite + React</h1>
