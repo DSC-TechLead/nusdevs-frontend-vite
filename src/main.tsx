7ad623a7a52cf1c7d34e239ea60c49338d0be59f
Stanley <wijayastanley00@gmail.com>
2024-12-21 15:25:40 +0700

    Add draggable test component

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import Components from "./pages/components.tsx";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Container } from "./components/test-container.tsx";
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Components />
-  </StrictMode>,
+  <DndProvider backend={HTML5Backend}>
+    <StrictMode>
+      {/* <Components /> */}
+      <Container />
+    </StrictMode>
+  </DndProvider>
 );
