5988487867735b60365ac8849f93d6710068ca18
Jason Ray <jasonraymoniaga@gmail.com>
2024-12-21 19:37:41 +0700

    Add Navbar UI/UX without functionality

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import Components from "./pages/components.tsx";
+import Navbar from "./components/navbar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Components />
+    <div className="mb-[95px]">
+      <Components />
+    </div>
+    <Navbar />
   </StrictMode>,
 );
