daf87afeb0a8ccdf8ab80555f01210c5f4b9f785
Stanley <wijayastanley00@gmail.com>
2025-01-06 22:03:49 +0700

    Add more inputs to page

--- src/components/hosting/general-info.tsx
+++ src/components/hosting/general-info.tsx
@@ -1,14 +1,28 @@
+import Dropdown from "../common/form/dropdown";
 import { FormHeader } from "../common/form/form-section";
+import TextInput from "../common/form/textinput";
 
 interface PrivacyPolicyProps {
   text?: string;
   isRead?: boolean;
 }
 
-const GeneralInfo: React.FC<PrivacyPolicyProps> = ({
-  text = "",
-  isRead = true,
-}) => {
+const dropdownSelections = [
+  {
+    label: "C1",
+    value: "Selection 1",
+  },
+  {
+    label: "C2",
+    value: "Selection 2",
+  },
+  {
+    label: "C3",
+    value: "Selection 3",
+  },
+];
+
+const GeneralInfo: React.FC<PrivacyPolicyProps> = ({}) => {
   const handleChange = () => {
     // onSelectedChange(!isRead);
   };
@@ -16,49 +30,29 @@ const GeneralInfo: React.FC<PrivacyPolicyProps> = ({
   return (
     <div className="w-full h-full px-5 py-8 bg-white rounded-2xl flex-col gap-7 inline-flex overflow-auto">
       <FormHeader header="General Info" subtitle="*Required Questions" />
-      <div className="p-2 border border-black overflow-auto flex-auto">
-        <p className="text- text-justify">{text}</p>
-      </div>
-      <div className="flex gap-3">
-        <div className="flex h-6 shrink-0 items-center">
-          <div className="group grid size-4 grid-cols-1">
-            <input
-              name="check-select"
-              type="checkbox"
-              checked={isRead}
-              onChange={() => handleChange()}
-              className="col-start-1 row-start-1 appearance-none rounded border 
-                  border-neutral-20 bg-white checked:border-primary 
-                  checked:bg-primary indeterminate:border-primary focus-visible:outline 
-                  focus-visible:outline-2 focus-visible:outline-offset-2 
-                  focus-visible:outline-iprimary disabled:border-neutral-20 
-                  disabled:bg-neutral-10 disabled:checked:bg-neutral-10 
-                  forced-colors:appearance-auto"
-            />
-            <svg
-              fill="none"
-              viewBox="0 0 14 14"
-              className="pointer-events-none col-start-1 row-start-1 size-3.5 
-                  self-center justify-self-center stroke-white 
-                  group-has-[:disabled]:stroke-gray-950/25"
-            >
-              <path
-                d="M3 8L6 11L11 3.5"
-                strokeWidth={1}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="opacity-0 group-has-[:checked]:opacity-100"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className="text-sm/6">
-          <p className="font-medium text-primary-text text-left">
-            I accept the <u>Privacy Policy</u> as well as the{" "}
-            <u>Terms & Conditions</u>.
-          </p>
-        </div>
-      </div>
+      <TextInput
+        label="Team Name*"
+        placeholder="E.G. Orbital Team"
+        description=""
+        maxLength={50}
+        value=""
+        handleInputChange={() => {}}
+      />
+      <Dropdown
+        label="Team Category*"
+        description="test"
+        options={dropdownSelections}
+        handleChange={() => {}}
+      />
+      <TextInput
+        label="Team Name*"
+        placeholder="E.G. Orbital Team"
+        description=""
+        maxLength={250}
+        value=""
+        handleInputChange={() => {}}
+        height={5}
+      />
     </div>
   );
 };
