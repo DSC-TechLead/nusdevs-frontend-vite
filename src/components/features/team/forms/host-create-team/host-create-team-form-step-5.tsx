ad5493e27472f714e5433d89a19556f81f2381c8
jmsandiegoo <jmsandiego0710@gmail.com>
2025-01-14 21:30:38 +0800

    Add step 5 basic ui

--- src/components/features/team/forms/host-create-team/host-create-team-form-step-5.tsx
+++ src/components/features/team/forms/host-create-team/host-create-team-form-step-5.tsx
@@ -1,6 +1,31 @@
+import { Card, CardDescription, CardTitle } from "@components/common/card";
+import { DndContext } from "@dnd-kit/core";
+import HostScreeningQuestionCard from "../../host-question-cards/host-screening-question-card";
+import { Button } from "@/components/common/button";
+import { HiPlus } from "react-icons/hi2";
+
 const HostCreateTeamFormStep5: React.FC = () => {
-  // TODO
-  return <div>Step 5</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <Card isDraggable={false} className="px-5 py-8 text-start">
+        <CardTitle className="py-4 text-h3">Screening Questions</CardTitle>
+        <CardDescription className="text-body-small text-secondary-text">
+          If you do not require screening questions, you can directly publish
+          your team
+        </CardDescription>
+      </Card>
+      <HostScreeningQuestionCard />
+      <Button className="bg-neutral">
+        <HiPlus /> Add Suggested Question
+      </Button>
+      <Button
+        variant="outline"
+        className="bg-white border-neutral text-neutral"
+      >
+        Add Question
+      </Button>
+    </div>
+  );
 };
 
 export default HostCreateTeamFormStep5;
