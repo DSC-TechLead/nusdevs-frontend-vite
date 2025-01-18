64a8956e0afb0b98f32da981feb322971ca6bca5
jmsandiegoo <jmsandiego0710@gmail.com>
2025-01-18 14:56:45 +0800

    Add question related type

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -1,3 +1,28 @@
-export const ScreeningQuestionType = {
-  DROPDOWN: "dropdown",
-};
+export enum QuestionType {
+  SHORT_ANSWER = 1,
+  LONG_ANSWER = 2,
+  DROPDOWN = 3,
+  RADIOBUTTON = 4,
+  CHECKBOX = 5,
+  FILEU_PLOAD = 6,
+}
+
+export interface Option {
+  label: string;
+  value: string | number;
+  disabled?: boolean;
+}
+
+export interface Validation {
+  required?: boolean;
+}
+
+export interface Question {
+  questionId: string;
+  formId: string;
+  questionType: QuestionType;
+  options?: Option[];
+  isRequired: boolean;
+  validation?: Validation;
+  question_order: number;
+}
