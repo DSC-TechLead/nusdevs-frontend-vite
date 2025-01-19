c706b963fbd6b581ecc7da36af5c89169e16fc43
jmsandiegoo <jmsandiego0710@gmail.com>
2025-01-19 11:10:29 +0800

    Fix minor bugs

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -2,7 +2,6 @@ import App from "@/App";
 import HostCreateTeamFormStep4 from "@/components/features/team/forms/host-create-team/host-create-team-form-step-4";
 import HostCreateTeamFormStep5 from "@/components/features/team/forms/host-create-team/host-create-team-form-step-5";
 import CreateTeamPage from "@/pages/create-team-page";
-import { Container } from "@/components/test-container";
 import Components from "@pages/components";
 import { BrowserRouter, Route, Routes } from "react-router";
 
@@ -16,7 +15,6 @@ const AppRoutes = () => {
           <Route path="step-4" element={<HostCreateTeamFormStep4 />} />
           <Route path="step-5" element={<HostCreateTeamFormStep5 />} />
         </Route>
-        <Route path="/drag-demo" element={<Container />} />
       </Routes>
     </BrowserRouter>
   );
