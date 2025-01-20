b86bedb451c2eccc3435d962a4f0582cd970151a
jmsandiegoo <jmsandiego0710@gmail.com>
2025-01-20 21:20:21 +0800

    Add text area component

---
+++ src/components/common/form/textarea.tsx
@@ -0,0 +1,16 @@
+import { forwardRef, TextareaHTMLAttributes } from "react";
+
+const TextAreaInput = forwardRef<
+  HTMLTextAreaElement,
+  TextareaHTMLAttributes<HTMLTextAreaElement>
+>((props, ref) => (
+  <div className="mt-2">
+    <textarea
+      ref={ref}
+      className="block w-full rounded-md px-3 py-1.5 text-base text-body-regular outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+      {...props}
+    />
+  </div>
+));
+
+export default TextAreaInput;
